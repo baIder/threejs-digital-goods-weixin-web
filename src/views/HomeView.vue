8e66de508af83c92a56e05bf8f671aaf23dd9f47
王云阳 <511763880@qq.com>
2023-07-21 09:56:05 +0800

    添加模型

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,49 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import * as THREE from "three";
+import { onMounted, ref } from "vue";
+
+const renderEl = ref<HTMLCanvasElement>();
+
+const init = () => {
+  const scene = new THREE.Scene();
+
+  const geometry = new THREE.BoxGeometry(2, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  const sizes = {
+    width: 800,
+    height: 600,
+  };
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    1000
+  );
+  scene.add(camera);
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas: renderEl.value,
+  });
+
+  renderer.setSize(sizes.width, sizes.height);
+
+  camera.position.z = 2;
+  camera.position.y = 0;
+
+  renderer.render(scene, camera);
+};
+
+onMounted(() => {
+  init();
+});
+</script>
 
 <template>
-  <h1>hello</h1>
+  <canvas ref="renderEl"></canvas>
 </template>
 
 <style scoped lang="scss"></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://192.168.36.2",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
